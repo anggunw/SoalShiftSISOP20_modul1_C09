ea7e85fa52ab08407f277436985cd80abc77ce7e
anggunw <anggunwahyuni51@gmail.com>
2020-02-20 22:14:25 +0700

    penyelesaian nomor 1

--- sisop1.sh
+++ sisop1.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-awk -v region="$reg" 'BEGIN{FS="\t"}
+awk 'BEGIN{FS="\t"}
 FNR>1{arr[$13]+=$21;reg=$13;min=arr[$13]}
 END{for(i in arr){if(arr[i]<min){min=arr[i];reg=i}};print reg > "jawaban1.var";print reg,min}' Sample-Superstore.tsv
 
